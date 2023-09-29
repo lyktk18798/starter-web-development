602b68b9ac3c4e6e5e93bb8b1d63f444afbb5d3c
TrucHuynh <nguyentruchuynh2002@gmail.com>
2023-09-29 22:28:34 +0700

    fix: Button Start game

--- dice-game/index.js
+++ dice-game/index.js
@@ -1,20 +1,20 @@
-// For Player1
-let randomNumber1 = Math.floor(Math.random() * 6) + 1;
-let randomImage1 = "images/dice" + randomNumber1 + ".png";
-document.querySelectorAll("img")[0].setAttribute("src", randomImage1); 
-
-// For Player2
-let randomNumber2 = Math.floor(Math.random() * 6) + 1;
-let randomImage2 = "images/dice" + randomNumber2 + ".png";
-document.querySelectorAll("img")[1].setAttribute("src", randomImage2);
-
-// For Winner
-if (randomNumber1 > randomNumber2) {
-    document.querySelector("h1").innerHTML = "Player 1 Win";
-}
-else if (randomNumber2 > randomNumber1) {
-    document.querySelector("h1").innerHTML = "Player 2 Win";
-}
-else {
-    document.querySelector("h1").innerHTML = "Equal!";
-}
+const btn = document.getElementById('btn');
+btn.addEventListener('click', function(){
+    let randomNumber1 = Math.floor(Math.random() * 6) + 1;
+    let randomImage1 = "images/dice" + randomNumber1 + ".png";
+    document.querySelectorAll("img")[0].setAttribute("src", randomImage1); 
+    
+    let randomNumber2 = Math.floor(Math.random() * 6) + 1;
+    let randomImage2 = "images/dice" + randomNumber2 + ".png";
+    document.querySelectorAll("img")[1].setAttribute("src", randomImage2);
+    
+    if (randomNumber1 > randomNumber2) {
+        document.querySelector("h1").innerHTML = "Player 1 Win";
+    }
+    else if (randomNumber2 > randomNumber1) {
+        document.querySelector("h1").innerHTML = "Player 2 Win";
+    }
+    else {
+        document.querySelector("h1").innerHTML = "Equal!";
+    }
+});
